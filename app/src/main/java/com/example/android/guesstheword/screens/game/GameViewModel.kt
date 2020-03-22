6c4cbac6de2e2acfe35c35de6a3456f32ab5f84d
brandonchua17 <61720822+brandonchua17@users.noreply.github.com>
2020-03-22 17:37:09 +0800

    sixth commit (5.4 coding challenge)

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -59,6 +59,14 @@ class GameViewModel : ViewModel() {
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
 
+    // The Hint for the current word
+    val wordHint = Transformations.map(word) { word ->
+        val randomPosition = (1..word.length).random()
+        "Current word has " + word.length + " letters" +
+                "\nThe letter at position " + randomPosition + " is " +
+                word.get(randomPosition - 1).toUpperCase()
+    }
+
     /**
      * Resets the list of words and randomizes the order
      */
